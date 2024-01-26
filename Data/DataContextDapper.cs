4121ffb8e09ae1e6b5c9d98dcfd725a3a891158e
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-01-26 20:40:17 -0300

    feat: creates default config and methods for dapper

---
+++ Data/DataContextDapper.cs
@@ -0,0 +1,34 @@
+using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace DotnetAPI
+{
+    public class DataContextDapper(IConfiguration config)
+    {
+
+        public IEnumerable<T> LoadData<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Query<T>(sql);
+        }
+
+        public T LoadDataSingle<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(config.GetConnectionString("DefaultConnection"));
+            return dbConnection.QuerySingle<T>(sql);
+        }
+
+        public bool ExecuteSql(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Execute(sql) > 0;
+        }
+
+        public int ExecuteSqlWithRowCount(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Execute(sql);
+        }
+    }
+}
