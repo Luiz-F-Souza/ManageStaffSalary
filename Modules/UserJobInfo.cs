77d0179744f6cc211b6591576af0339026bc9f8b
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-01-26 20:38:55 -0300

    feat: creates UserJobInfo model

---
+++ Modules/UserJobInfo.cs
@@ -0,0 +1,10 @@
+namespace DotnetAPI
+{
+    public partial class UserJobInfo
+    {
+        public int UserId { get; set; }
+        public required string JobTitle { get; set; }
+        public required string Department { get; set; }
+
+    }
+}
