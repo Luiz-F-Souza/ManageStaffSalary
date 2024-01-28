7cbde2ce3636f5a09001667a6baa8a2f97770e70
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-01-28 13:51:54 -0300

    feat: creates put and post routes for handle edit and create user

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,7 +11,8 @@ public class UserController(IConfiguration config) : ControllerBase
 
 
     [HttpGet("TestConnection")]
-    public DateTime TestConnection(){
+    public DateTime TestConnection()
+    {
         return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
     }
 
@@ -25,11 +26,11 @@ public class UserController(IConfiguration config) : ControllerBase
 
         IEnumerable<User> Users = _dapper.LoadData<User>(sql);
 
-        
+
         return Users;
     }
 
-        // Se eu não passar aqui o /{userId} e deixar apenas no método ele vira um searchParam
+    // Se eu não passar aqui o /{userId} e deixar apenas no método ele vira um searchParam
     [HttpGet("GetSingleUser/{userId}")]
     // public IActionResult Test()
     public User GetSingleUser(int userId)
@@ -39,6 +40,51 @@ public class UserController(IConfiguration config) : ControllerBase
         User ReturnedUser = _dapper.LoadDataSingle<User>(sql);
 
         return ReturnedUser;
+    }
+
+
+    [HttpPut("EditUser")]
+    // Precisa ser um model para inferir que desejamos receber um json no body
+    public IActionResult EditUser(User user)
+    {
+
+        string sql = $@"
+            UPDATE TutorialAppSchema.Users
+            SET 
+                [FirstName] = '{user.FirstName}',
+                [LastName]  = '{user.LastName}',
+                [Email]     = '{user.Email}',
+                [Gender]    = '{user.Gender}',
+                [Active]    = {(user.Active ? 1 : 0)}
+            WHERE UserId = {user.UserId}
+        ".Trim();
+        bool HasFinishedWithSuccess = _dapper.ExecuteSql(sql);
+
+        return HasFinishedWithSuccess ? Ok() : throw new Exception("Não foi possível editar o usuário");
+    }
+
+    [HttpPost("AddUser")]
+    public IActionResult AddUser(CreateUserDTO user)
+    {
+        string sql = $@"
+            INSERT INTO TutorialAppSchema.Users(
+                [FirstName],
+                [LastName],
+                [Email],
+                [Gender],
+                [Active]
+            ) VALUES (
+               '{user.FirstName}',
+               '{user.LastName}',
+               '{user.Email}',
+               '{user.Gender}',
+               {(user.Active ? 1 : 0)}
+            )
+        ".Trim();
+
+        bool HasFinishedWithSucess = _dapper.ExecuteSql(sql);
+
+        return HasFinishedWithSucess ? Ok() : throw new Exception("Não foi possível criar o usuário");
     }
 
 }
